73a8b18e348d51133dea39c8ca3b6245a605a503
cecs18c29 <jesswinv@gmail.com>
2020-01-31 12:35:25 +0530

    customised the script in the folder

--- 31Jan20/sample.py
+++ 31Jan20/sample.py
@@ -4,7 +4,21 @@ failed = 0
 studdic={}
 
 def cgpacal(grades):
-	print(grades)
+	point=0
+	for i in grades:
+		if("(O)" in i):
+			point+=10
+		elif("(A+)" in i):
+			point+=9
+		elif("(A)" in i):
+                        point+=9
+		elif("(B+)" in i):
+                        point+=6
+		elif("(B)" in i):
+                        point+=6
+		elif("(D)" or "(C)" in i):
+                        point+=5
+	return(point/float(len(grades)))
 
 
 with open(fname,"r+") as f:
@@ -14,5 +28,5 @@ with open(fname,"r+") as f:
 		else:
 			studdic[i.split()[0]]=cgpacal(i.split()[1:]) 
 
-print(failed)
 pprint.pprint(studdic)
+print(failed)
