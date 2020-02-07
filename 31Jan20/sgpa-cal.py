98c0fc928a51adba7e2905869006e8be8d984d7a
cecs18c29 <jesswinv@gmail.com>
2020-02-07 10:57:47 +0530

    perfected the script and made some changes

--- 31Jan20/sgpa-cal.py
+++ 31Jan20/sgpa-cal.py
@@ -11,22 +11,24 @@ def cgpacal(grades):
 		elif("(A+)" in i):
 			point+=9
 		elif("(A)" in i):
-                        point+=9
+                        point+=8.5
 		elif("(B+)" in i):
-                        point+=6
+                        point+=8
 		elif("(B)" in i):
+                        point+=7
+		elif("(C)" in i):
                         point+=6
-		elif("(D)" or "(C)" in i):
+		elif("(P)" in i):
                         point+=5
+
 	return("{:.2f}".format(point/float(len(grades))))
 
 
 with open(fname,"r+") as f:
 	for i in f:
 		if(re.search("(F)", i)):
-			failed+=1
+			studdic[i.split()[0]] =-len(re.findall("(F)",i))
 		else:
 			studdic[i.split()[0]]=cgpacal(i.split()[1:]) 
 for student in studdic.keys():
 	print("{0}\n{1}\n".format(student,studdic[student]))
-print("FAILED:>{}".format(failed))
